ab4d972cfb4aec56f273205f4766247fd75afa52
Darren Maglaque <darren.maglaque@gmail.com>
2018-12-03 16:16:44 +0800

    ADded functionalities in catalog page

--- app/views/product.php
+++ app/views/product.php
@@ -3,7 +3,7 @@
 	$page_title = "Product Information";
 	require "../partials/header.php";
 	require "../controllers/get_product.php";
-
+	extract($product);
  ?>
 
  <?php require "../partials/navbar.php"; ?>
@@ -11,10 +11,16 @@
 	<main id="main" role="main">
 		<?php if ($product !== NULL): ?>
 			<h1>Product Information</h1>
+			<h2><?php echo $item_name ?></h2>
+			<h2><?php echo $description ?></h2>
+			<h2><?php echo $price ?></h2>
+			<h2><img src="../assets/<?php echo $img_path; ?>" alt=""></h2>
+			
 		<?php else: ?>
 			<h1>Product is non-existent</h1>	
 		<?php endif ?>
 	</main>
 
+	
 
  <?php require "../partials/footer.php"; ?>
